0ee8d4a30d352a7a3895be3bfc9de2fc866f5716
Shaheer Raja <81824949+sraja7272@users.noreply.github.com>
2025-11-10 20:22:19 -0600

    Add DuasPanel component and toggle functionality in App.jsx; enhance CSS for Arabic font support

---
+++ src/data/commonDuas.js
@@ -0,0 +1,115 @@
+export const commonDuas = [
+  {
+    id: 1,
+    title: "Du'a before Starting your Journey",
+    description: "Reciting the Du’a as Safar invokes Allah's protection and guidance",
+    arabic: "بِسْمِ اللهِ تَوَكَّلْـتُ عَلى اللهِ وَلا حَوْلَ وَلا قُـوَّةَ إِلاّ بِالله",
+    transliteration: "Bismillah, tawakkaltu 'ala Allah, wo la howla wo la quwwata illa billah",
+    meaning: "In the Name of Allah; I trust in Allah; No change in circumstances occurs except through the Power of Allah"
+  },
+  {
+    id: 2,
+    title: "Du'a for boarding a Vehicle",
+    description: "Recite this dua when boarding any vehicle",
+    arabic: "اَللهُ أَكْبَرُ ، اَللهُ أَكْبَرُ ، اَللهُ أَكْبَرُ ، سُبْحَانَ الَّذِيْ سَخَّرَ لَنَا هٰذَا وَمَا كُنَّا لَهُ مُقْرِنِيْنَ ، وَإِنَّا إِلَىٰ رَبِّنَا لَمُنْقَلِبُوْنَ ، اَللّٰهُمَّ إِنَّا نَسْأَلُكَ فِيْ سَفَرِنَا هٰذَا الْبِرَّ وَالتَّقْوَىٰ ، وَمِنَ الْعَمَلِ مَا تَرْضَىٰ ، اَللّٰهُمَّ هَوِّنْ عَلَيْنَا سَفَرَنَا هٰذَا وَاطْوِ عَنَّا بُعْدَهُ ، اَللّٰهُمَّ أَنْتَ الصَّاحِبُ فِي السَّفَرِ ، وَالْخَلِيْفَةُ فِي الْأَهْلِ ، اَللّٰهُمَّ إِنِّيْ أَعُوْذُ بِكَ مِنْ وَعْثَاءِ السَّفَرِ ، وَكَآبَةِ الْمَنْظَرِ ، وَسُوْءِ الْمُنْقَلَبِ فِيْ الْمَالِ وَالْأَهْلِ",
+    transliteration: " Allāhu akbar, Allāhu akbar, Allāhu akbar, subḥāna-l-ladhī sakkhara lanā hādhā wa mā kunnā lahū muqrinīn, wa innā ilā rabbinā la-munqalibūn, Allāhumma innā nas'aluka fī safarinā hādhā-l-birra wa-t-taqwā, wa mina-l-ʿamali mā tarḍā, Allāhumma hawwin ʿalaynā safaranā hādhā wa-ṭ-wi' ʿannā buʿdah, allāhumma Anta-ṣ-ṣāḥibu fi-s-safar, wa-l-khalīfatu fi-l-ahl, allāhumma innī aʿūdhu bika min waʿthāi-s-safar, wa kaābati-l-manẓar, wa sū'i-l-munqalabi fi-l-māli wa-l-ahl.",
+    meaning: "Allah is the greatest. Allah is the greatest. Allah is the greatest. How perfect is the one who has given us control over this; we could not have done it by ourselves. Truly, it is to our Lord that we are returning. O Allah, we ask you for piety, taqwā, and deeds which you will be pleased with on this journey of ours. O Allah, make this journey easy for us and let us cover its distance swiftly. O Allah, you are our companion on the journey and the One in whose care we leave our family. O Allah, I seek your protection from the difficulties of the journey, from distressing scenes, and an ill-fated outcome with my wealth and family"
+  },
+  {
+    id: 3,
+    title: "Du'a upon arrival",
+    description: "Recite this dua when arriving at your destination",
+    arabic: "أَعُوْذُ بِكَلِمَاتِ اللّٰهِ التَّامَّاتِ مِنْ شَرِّ مَا خَلَقَ",
+    transliteration: "A'uzu bikalimatillahit-t'ammati min sharri ma khalaq",
+    meaning: "I seek protection in Allah's perfect words from all evil (that has been created)"
+  },
+  {
+    id: 4,
+    title: "Du'a before entering Ihram",
+    description: "Recite this dua before entering the state of Ihram for Hajj or Umrah",
+    arabic: "سُبْحَانَ اللّٰهِ، لَا إِلٰهَ إِلَّا اللّٰهُ، اَللّٰهُ أَكْبَرُ",
+    transliteration: "Subhanallahi, La ilaha illallahu, Allahu Akbar",
+    meaning: "All praise is due to Allah, there is no god but Allah, and Allah is the Greatest"
+  },
+  {
+    id: 5,
+    title: "Du’a for making the intention for performing Umrah",
+    description: "Once in Ihram, perform two rakahs and make the following intention",
+    arabic: "اللَّهُمَّ إِنِّيْ أُرِيْدُ الْعُمْرَةَ فَيَسِّرْهَا لِيْ وَتَقَبَّلْهَا مِنِّيْ",
+    transliteration: "Allahumma inni uridul umrah; fayassir haa li wa taqabbalhaa minni",
+    meaning: "O Allah! I want to do Umrah. Please make it easy for me and accept it"
+  },
+  {
+    id: 6,
+    title: "Talbiyah for Umrah",
+    description: "Reciting the Talbiyah frequently is encouraged",
+    arabic: "لَبَّيْكَ اللهُمَّ لَبَّيْكَ",
+    transliteration: "Labbayka Allahumma Labbayk",
+    meaning: "Here I am at your service, O Allah"
+  },
+  {
+    id: 7,
+    title: "Du’a as you enter the Masjid Al Haram",
+    description: "Recite this dua when entering the Masjid Al Haram",
+    arabic: "بِسْـمِ الله وَالصَّلاةُ وَالسَّلامُ عَلى رَسولِ الله اللّهُـمَّ افْتَـحْ لي أَبْوابَ رَحْمَتـِك",
+    transliteration: "Bismillahi wal hamdulillahi wassalatu a’laa Rasulillah, Allahummaghfirlee zunoobee waf tahlee abwaaba rahmatik",
+    meaning: "I start with the name of Allah; all praise is for Him, and blessings upon the Prophet of Allah. O Allah, forgive my sins and open the doors of Your mercy for me"
+  },
+  {
+    id: 8,
+    title: "Du’a for the first sight of Kaaba",
+    description: "Recite this dua when you see the Kaaba for the first time",
+    arabic: "اَللَّهُمَّ زِدْ هَذَا الْبَيْتَ تَشْرِيفاً وَتَعْظِيماً",
+    transliteration: "Allahumma zid hadha l-bayta tashrifan wa ta’dhiman",
+    meaning: "O Allah, make this House more honored and respected"
+  },
+  {
+    id: 9,
+    title: "Du'a to recite during Tawaf",
+    description: "Recite this dua while performing Tawaf around the Kaaba",
+    arabic: "بِسْمِ اللهِ اللَّه أكْبَر وللّهِ الْحَمْدُ",
+    transliteration: "Bismillahi Allahu Akbar wa lillahil hamd",
+    meaning: "I start in the name of Allah, Allah is the Greatest, and all praise belongs to Him"
+  },
+  {
+    id: 10,
+    title: "Du’a from the Yemeni corner to the Black Stone",
+    description: "Recite this each round from the Yemeni corner to the Black Stone during Tawaf",
+    arabic: "رَبَّنَا آتِنَا فِىْ الدُّنْيَا حَسَنَةً وَفِىْ الآخِرَةِ حَسَنَةً وَّقِنَا عَذَابَ النَّارِ",
+    transliteration: "Rabbanā ātinā fī d-dunyā hasana wa fī l-Ākhirati hasana wa qinā ‘adhāban-Nār",
+    meaning: "Our Lord! Grant us goodness in this world and in the here after, and protect us from the torment of the Fire"
+  },
+  {
+    id: 11,
+    title: "Du’a for drinking Zamzam",
+    description: "Recite this dua before drinking Zamzam water",
+    arabic: "اللَّهُمَّ إِنِّي أَسْأَلُكَ عِلْمَاً نَافِعَاًً وَرِزْقَاً وَاسِعَاًَ وَشِفَاءً مِنْ كُلِّ دَاءٍ",
+    transliteration: "Allahumma inni as'aluka ilman nafi'an wa rizqan wasi'an wa shifa'an min kulli da'in",
+    meaning: "O Allah, I seek from you beneficial knowledge, abundant sustenance, and healing from all ailments"
+  },
+  {
+    id: 12,
+    title: "Du'a for Leaving The Masjid",
+    description: "Recite this dua when leaving any masjid",
+    arabic: "اللهم إني أسألك من فضلك",
+    transliteration: "As-salamu ala rasulullah allahumma Inni as aluka min Fadlik",
+    meaning: "Peace be upon the messenger of Allah (saw). O Allah, I ask you (to grant me) from your favor"
+  },
+  {
+    id: 13,
+    title: "Du'a for Sa'ee",
+    description: "Recite this dua before beginning Sa'ee (At the foot of Safā)",
+    arabic: "إنَّ الصَّفَا وَالْمَرْوَةَ مِنْ شَعَآئِرِ اللَّهِ فَمَنْ حَجَّ الْبَيْتَ أوِ اعْتَمَرَ فَلاَ جُنَاحَ عَلَيْهِ أَنْ يَطَّوَّفَ بِهِمَا وَمَنْ تَطَوَّعَ خَيْراً فَإنَّ اللَّهَ شَاكِرٌ عَلِيْم ‐ نَبْدَأ بِمَا بَدَأ اللَّهُ بِهِ",
+    transliteration: "Inna s-Safa wal-Marwah min sha'a'irillah faman hajjal-bayta aw i'tamara fala junaha 'alaihi an yattawwafa bihimā wa man tatawwa'a khairan fa innallāha shākirun 'aleem. Nabda'u bima bada'a Allāhu bihi",
+    meaning: "Verily! As-Safa and Al-Marwah are from the signs of Allah. It is not a sin for him who performs Hajj or 'Umrah to walk between them. Whoever does good voluntarily, then verily, Allah is All-Recogniser, All-Knower"
+  },
+  {
+    id: 14,
+    title: "Du’a to recite at the tops of Safa and Marwah",
+    description: "Recite this dua at the tops of Safa and Marwah during Sa'ee",
+    arabic: "اللَّهُ أكْبَرُ، اللَّهُ أكْبَرُ، اللَّهُ أكْبَرُ لاَ إلَهَ إلاَّ اللَّهُ وَحْدَهُ لاَ شَرِيْكَ لَهُ، لَهُ الْمُلْكُ وَ لَهُ الْحَمْدُ يُحْيِي وَ يُمِيْتُ وَ هُوَ عَلَى كُلِّ شَيْءٍ قَدِيْر؛ لاَ إلَهَ إلاَّ اللَّهُ وَحْدَهُ لاَ شَرِيْكَ لَه، أنْجَزَ وَعْدَهُ وَ نَصَرَ عَبْدَهُ وَ هَزَمَ الأحْزَابَ وَ حْدَهُ",
+    transliteration: "Allahu Akbar, Allahu Akbar, Allahu Akbar. Lā ilaha illallah, wahdahu lā sharīka lah, lahul mulk, wa lahul-hamd, yuhyi wa yumit, wa huwa ‘alā kulli shay’in qadeer; lā ilāha illallāh wahdahu lā sharīka lah, anjaza wa’dahu, wa nasara ‘abdahu, wa hazama l-ahzāba wahdah",
+    meaning: "Allah is the Greatest, Allah is the Greatest, Allah is the Greatest. There is no one worthy of worship except Allah, alone with no partner. He has all power and praise. He gives life and causes death, and He has control over everything. There is no one worthy of worship except Allah, alone with no partner. He kept His promise, helped His servant, and defeated the enemies by Himself"
+  }
+]
+
